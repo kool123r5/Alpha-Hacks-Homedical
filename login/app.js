6f54cc1e8dc258d0404bf7e1b0a8be28b9ba2ff5
Kushal <74308033+kool123r5@users.noreply.github.com>
2021-06-12 21:24:35 +0530

    Update app.js

--- login/app.js
+++ login/app.js
@@ -1,1 +1,15 @@
+    const auth = firebase.auth();
 
+    const signInBtn = document.getElementById('signInBtn');
+    const provider = new firebase.auth.GoogleAuthProvider();
+    var db = firebase.firestore();
+
+    signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+    auth.onAuthStateChanged(user => {
+        if (user) {
+            window.location = 'homepage.html'
+        } else {
+            
+        }
+    })
