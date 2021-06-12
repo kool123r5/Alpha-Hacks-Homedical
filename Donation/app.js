19e37b7100d2de0220f7388e0c546c6889b70290
Kushal <74308033+kool123r5@users.noreply.github.com>
2021-06-12 21:12:39 +0530

    Update app.js

--- Donation/app.js
+++ Donation/app.js
@@ -1,1 +1,38 @@
+var db = firebase.firestore();
 
+let unsubscribe;
+const customList = document.getElementById('customList');
+var customRef = db.collection('donations');
+
+const cancerDonateButton = document.getElementById('cancerDonateButton');
+const covidDonateButton = document.getElementById('covidDonateButton');
+
+var init = function(){
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          console.log(firebase.auth().currentUser);
+
+          cancerDonateButton.onclick = () => {
+              window.location = 'donationButtonClicked.html'
+          }
+
+          covidDonateButton.onclick = () => {
+              window.location = 'donationButtonClicked.html'
+          }
+
+        } else {
+          window.location.replace("index.html");
+        }
+      });
+}
+init();
+
+unsubscribe = customRef
+        .orderBy('amount')
+        .onSnapshot(querySnapshot => {
+            const items = querySnapshot.docs.map(doc => {
+                return `<li>${doc.data().name} donated ${doc.data().amount}</li>`
+            });
+
+            customList.innerHTML = items.join('');
+    });
