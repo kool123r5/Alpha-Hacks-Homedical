15e69233bc3c3fd929c72446a2e019231252edaf
Kushal <74308033+kool123r5@users.noreply.github.com>
2021-06-13 11:36:48 +0530

    Update homepage.js

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,8 +1,10 @@
 let bg = document.getElementById("bg");
+const auth = firebase.auth();
 let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
 let road = document.getElementById("road");
 let text = document.getElementById("text");
+const logOutButton = document.getElementById('logOutButton');
 
 window.addEventListener('scroll', function(){
   var value = window.scrollY;
@@ -12,7 +14,6 @@ window.addEventListener('scroll', function(){
   moon.style.left  = -value * 0.5 + 'px';
   road.style.top  = value * 0.15 + 'px';
   mountain.style.top  = -value * 1 + 'px';
- 
- 
-  
 })
+
+logOutButton.onclick = () => auth.signOut();
